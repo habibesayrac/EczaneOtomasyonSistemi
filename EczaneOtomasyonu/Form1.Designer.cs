2d831ce02517065678de05a31aca62ba2a758df4
habibesayrac <habibesayrac@outlook.com>
2023-12-25 10:57:11 +0300

    Add project files.

---
+++ EczaneOtomasyonu/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace EczaneOtomasyonu
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.hastalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hastaKaydıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hastalarıListeleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hastaSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hastaGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ilaçlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ilaçKaydıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ilaçlarıListeleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ilaçSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ilaçGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.güvenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.güvenceEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.güvenceleriListeleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.güvenceGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+			this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hastalarToolStripMenuItem,
+            this.ilaçlarToolStripMenuItem,
+            this.güvenceToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1270, 33);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// hastalarToolStripMenuItem
+			// 
+			this.hastalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hastaKaydıToolStripMenuItem,
+            this.hastalarıListeleToolStripMenuItem,
+            this.hastaSilToolStripMenuItem,
+            this.hastaGüncelleToolStripMenuItem});
+			this.hastalarToolStripMenuItem.Name = "hastalarToolStripMenuItem";
+			this.hastalarToolStripMenuItem.Size = new System.Drawing.Size(92, 29);
+			this.hastalarToolStripMenuItem.Text = "Hastalar";
+			// 
+			// hastaKaydıToolStripMenuItem
+			// 
+			this.hastaKaydıToolStripMenuItem.Name = "hastaKaydıToolStripMenuItem";
+			this.hastaKaydıToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.hastaKaydıToolStripMenuItem.Text = "Hasta Kaydı";
+			// 
+			// hastalarıListeleToolStripMenuItem
+			// 
+			this.hastalarıListeleToolStripMenuItem.Name = "hastalarıListeleToolStripMenuItem";
+			this.hastalarıListeleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.hastalarıListeleToolStripMenuItem.Text = "Hastaları Listele";
+			// 
+			// hastaSilToolStripMenuItem
+			// 
+			this.hastaSilToolStripMenuItem.Name = "hastaSilToolStripMenuItem";
+			this.hastaSilToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.hastaSilToolStripMenuItem.Text = "Hasta Sil";
+			// 
+			// hastaGüncelleToolStripMenuItem
+			// 
+			this.hastaGüncelleToolStripMenuItem.Name = "hastaGüncelleToolStripMenuItem";
+			this.hastaGüncelleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.hastaGüncelleToolStripMenuItem.Text = "Hasta Güncelle";
+			// 
+			// ilaçlarToolStripMenuItem
+			// 
+			this.ilaçlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ilaçKaydıToolStripMenuItem,
+            this.ilaçlarıListeleToolStripMenuItem,
+            this.ilaçSilToolStripMenuItem,
+            this.ilaçGüncelleToolStripMenuItem});
+			this.ilaçlarToolStripMenuItem.Name = "ilaçlarToolStripMenuItem";
+			this.ilaçlarToolStripMenuItem.Size = new System.Drawing.Size(73, 29);
+			this.ilaçlarToolStripMenuItem.Text = "İlaçlar";
+			// 
+			// ilaçKaydıToolStripMenuItem
+			// 
+			this.ilaçKaydıToolStripMenuItem.Name = "ilaçKaydıToolStripMenuItem";
+			this.ilaçKaydıToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.ilaçKaydıToolStripMenuItem.Text = "İlaç Kaydı";
+			// 
+			// ilaçlarıListeleToolStripMenuItem
+			// 
+			this.ilaçlarıListeleToolStripMenuItem.Name = "ilaçlarıListeleToolStripMenuItem";
+			this.ilaçlarıListeleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.ilaçlarıListeleToolStripMenuItem.Text = "İlaçları Listele";
+			// 
+			// ilaçSilToolStripMenuItem
+			// 
+			this.ilaçSilToolStripMenuItem.Name = "ilaçSilToolStripMenuItem";
+			this.ilaçSilToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.ilaçSilToolStripMenuItem.Text = "İlaç Sil";
+			// 
+			// ilaçGüncelleToolStripMenuItem
+			// 
+			this.ilaçGüncelleToolStripMenuItem.Name = "ilaçGüncelleToolStripMenuItem";
+			this.ilaçGüncelleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.ilaçGüncelleToolStripMenuItem.Text = "İlaç Güncelle";
+			// 
+			// güvenceToolStripMenuItem
+			// 
+			this.güvenceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.güvenceEkleToolStripMenuItem,
+            this.güvenceleriListeleToolStripMenuItem,
+            this.güvenceGüncelleToolStripMenuItem});
+			this.güvenceToolStripMenuItem.Name = "güvenceToolStripMenuItem";
+			this.güvenceToolStripMenuItem.Size = new System.Drawing.Size(95, 29);
+			this.güvenceToolStripMenuItem.Text = "Güvence";
+			// 
+			// güvenceEkleToolStripMenuItem
+			// 
+			this.güvenceEkleToolStripMenuItem.Name = "güvenceEkleToolStripMenuItem";
+			this.güvenceEkleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.güvenceEkleToolStripMenuItem.Text = "Güvence Ekle";
+			// 
+			// güvenceleriListeleToolStripMenuItem
+			// 
+			this.güvenceleriListeleToolStripMenuItem.Name = "güvenceleriListeleToolStripMenuItem";
+			this.güvenceleriListeleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.güvenceleriListeleToolStripMenuItem.Text = "Güvenceleri Listele";
+			// 
+			// güvenceGüncelleToolStripMenuItem
+			// 
+			this.güvenceGüncelleToolStripMenuItem.Name = "güvenceGüncelleToolStripMenuItem";
+			this.güvenceGüncelleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.güvenceGüncelleToolStripMenuItem.Text = "Güvence Güncelle";
+			// 
+			// çıkışToolStripMenuItem
+			// 
+			this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+			this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+			this.çıkışToolStripMenuItem.Text = "Çıkış";
+			this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(24, 114);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 62;
+			this.dataGridView1.RowTemplate.Height = 28;
+			this.dataGridView1.Size = new System.Drawing.Size(1187, 138);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(20, 67);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(119, 20);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "HastaNo(TcNo)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(328, 67);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "BarkodNo";
+			// 
+			// maskedTextBox1
+			// 
+			this.maskedTextBox1.Location = new System.Drawing.Point(145, 64);
+			this.maskedTextBox1.Mask = "0000000000";
+			this.maskedTextBox1.Name = "maskedTextBox1";
+			this.maskedTextBox1.Size = new System.Drawing.Size(170, 26);
+			this.maskedTextBox1.TabIndex = 5;
+			this.maskedTextBox1.ValidatingType = typeof(int);
+			// 
+			// maskedTextBox2
+			// 
+			this.maskedTextBox2.Location = new System.Drawing.Point(414, 64);
+			this.maskedTextBox2.Mask = "00000";
+			this.maskedTextBox2.Name = "maskedTextBox2";
+			this.maskedTextBox2.Size = new System.Drawing.Size(170, 26);
+			this.maskedTextBox2.TabIndex = 6;
+			this.maskedTextBox2.ValidatingType = typeof(int);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(648, 64);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(75, 26);
+			this.numericUpDown1.TabIndex = 7;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(585, 67);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 20);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Adet";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(810, 62);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(170, 31);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Satış Yap";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1270, 466);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.maskedTextBox2);
+			this.Controls.Add(this.maskedTextBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.menuStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ECZANE OTOMASYONU";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem hastalarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hastaKaydıToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hastalarıListeleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hastaSilToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hastaGüncelleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ilaçlarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ilaçKaydıToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ilaçlarıListeleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ilaçSilToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ilaçGüncelleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem güvenceToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem güvenceEkleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem güvenceleriListeleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem güvenceGüncelleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+		private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ EczaneOtomasyonu/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace EczaneOtomasyonu
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+		Application.Exit();
+		}
+		void listele()
+		{
+			OleDbConnection connection = new OleDbConnection("provider=microsoft.ace.oledb.12.0; data source=EczaneDb.accdb");
+			OleDbCommand komut = new OleDbCommand("Select * from satislar",connection);
+			OleDbDataAdapter da = new OleDbDataAdapter(komut);
+			DataTable tablo = new DataTable();
+			da.Fill(tablo);
+			dataGridView1.DataSource= tablo;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			listele();
+		}
+	}
+}
